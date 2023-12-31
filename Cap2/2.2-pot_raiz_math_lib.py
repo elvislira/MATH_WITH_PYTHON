9672038d4dfff59ff04a0c2ce1a37279b6c71531
Elvis Lira <professorelvislira@gmail.com>
2023-12-31 17:38:36 -0300

    Potenciação e radiciação.

---
+++ Cap2/2.2-pot_raiz_math_lib.py
@@ -0,0 +1,16 @@
+from math import pow
+from math import sqrt
+
+
+# Potenciação usando operadores
+x = 3
+y = 5
+
+potencia = pow(x, y)
+print(f'{potencia}({type(potencia)})')
+
+# Radiciação usando operadores
+z = 81
+
+raiz_quadrada = sqrt(z)
+print(f'{raiz_quadrada}({type(raiz_quadrada)})')
---
+++ Cap2/2.2-pot_raiz_operadores.py
@@ -0,0 +1,12 @@
+# Potenciação usando operadores
+x = 3
+y = 5
+
+potencia = x ** y
+print(f'{potencia}({type(potencia)})')
+
+# Radiciação usando operadores
+z = 81
+
+raiz_quadrada = z ** (1/2)
+print(f'{raiz_quadrada}({type(raiz_quadrada)})')
