c90cd7f33f92fd5a4ded658079942591268a2ca8
Elvis Lira <professorelvislira@gmail.com>
2024-01-02 15:28:21 -0300

    Operações com frações.

---
+++ Cap2/2.6-fracoes.py
@@ -0,0 +1,14 @@
+from fractions import Fraction
+
+x = Fraction(2, 3)
+y = Fraction(2.5)
+z = Fraction('1.4')
+w = Fraction(' -1/5 ')
+
+print(f'x = {x}')
+print(f'y = {y}')
+print(f'z = {z}')
+print(f'w = {w}')
+
+print(f'z + w = {z+w}')
+print(f'({x})^2 = {x**2}')
