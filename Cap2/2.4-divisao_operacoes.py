76b560d620e80b17c9ec23c8a052265e41a5dc12
Elvis Lira <professorelvislira@gmail.com>
2024-01-02 11:58:55 -0300

    Operações com divisão.

---
+++ Cap2/2.4-divisao_operacoes.py
@@ -0,0 +1,11 @@
+x = 10.0
+y = 3.0
+
+divisao_ponto_flutuante = x / y
+divisao_inteira = x // y
+resto_divisao = x % y
+
+
+print(f'Divisão ponto flutuante: {divisao_ponto_flutuante:.2f}')
+print(f'Divisão inteira: {divisao_inteira}')
+print(f'Resto da divisão: {resto_divisao}')
