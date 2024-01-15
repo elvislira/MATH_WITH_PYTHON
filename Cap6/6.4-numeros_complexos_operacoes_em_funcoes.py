925106c94a0fc75efc650ddbc4e03c6b33a2a759
Elvis Lira <professorelvislira@gmail.com>
2024-01-15 16:05:37 -0300

    Operações em funções.

---
+++ Cap6/6.4-numeros_complexos_operacoes_em_funcoes.py
@@ -0,0 +1,16 @@
+from cmath import pi
+from cmath import exp
+from cmath import log
+from cmath import sin
+from cmath import cos
+from cmath import tan
+
+
+z = 4 - 7j
+
+print(f'z = {z}')
+print(f'Exponencial de z = {exp(z):.1f}')
+print(f'Logaritmo de z na base 3 = {log(z, 3):.1f}')
+print(f'Seno de z = {sin(z):.1f}')
+print(f'Coseno de z = {cos(z):.1f}')
+print(f'Tangente de z = {tan(z):.1f}')
