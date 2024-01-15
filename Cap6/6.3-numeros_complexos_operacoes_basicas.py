055895f2b393987b20d48dfdcefe09465071e20d
Elvis Lira <professorelvislira@gmail.com>
2024-01-15 16:05:09 -0300

    Operações básicas.

---
+++ Cap6/6.3-numeros_complexos_operacoes_basicas.py
@@ -0,0 +1,17 @@
+from cmath import sqrt
+from cmath import pi
+from cmath import exp
+
+
+z = 2 - 5j
+w = 3 + 2j
+
+# Soma
+print(f'z + w = {z + w}')
+print(f'z - w = {z - w}')
+print(f'z * w = {z * w}')
+print(f'z / w = {z / w}')
+print(f'z² = {z ** 2}')
+print(f'w² = {w ** 2}')
+print(f'Raiz quadrada de z: {sqrt(z)}')
+print(f'Raiz quadrada de w: {sqrt(w)}')
